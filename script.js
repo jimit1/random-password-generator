aaf8c87aab168ec2ded3e54525566e326dc68b5d
Jim Mehta <jimit1@gmail.com>
2020-04-17 22:00:52 -0700

    fully working password generator. Also validates that it has minimum 1 number and 1 special character if asked for

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ var upperCaseChar = [
   "Z",
 ];
 
-var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var specialChar = [
   "!",
@@ -109,17 +109,30 @@ function userPrompts() {
     "How long of a password do you want? Enter number between 8 and 128"
   );
   passwordLength = parseInt(userLength);
-  userLowerCase = confirm(
-    "Do you require Lower Case Characters in your password?"
-  );
-  userUpperCase = confirm(
-    "Do you require Upper Case Characters in your password?"
-  );
-  userNumCase = confirm("Do you require Numbers in you password");
-  userSpecialCase = confirm(
-    "Do you require Special Case Characters in your password?"
-  );
 
+  if (passwordLength > 7 && passwordLength < 129) {
+    userLowerCase = confirm(
+      "Do you require Lower Case Characters in your password?"
+    );
+    userUpperCase = confirm(
+      "Do you require Upper Case Characters in your password?"
+    );
+    userNumCase = confirm("Do you require Numbers in your password");
+    userSpecialCase = confirm(
+      "Do you require Special Case Characters in your password?"
+    );
+  } else {
+    showError();
+  }
+
+  if (
+    userLowerCase === false &&
+    userUpperCase === false &&
+    userNumCase === false &&
+    userSpecialCase === false
+  ) {
+    showError();
+  }
   return passwordLength;
   return userLowerCase;
   return userUpperCase;
@@ -150,17 +163,30 @@ function randCharGenerator() {
   return randMasterChar;
 }
 
-function showPassword() {
-  writePassword.value = password;
-}
-
-function showError() {
-  writePassword.value = "Invalid Input. Hit Generate Password again";
+function generatePassword() {
+  var tempPass = [];
+  if (passwordLength > 7 && passwordLength < 129) {
+    if (
+      userLowerCase !== false &&
+      userUpperCase !== false &&
+      userNumCase !== false &&
+      userSpecialCase !== false
+    ) {
+      for (var i = 0; i < passwordLength; i++) {
+        randCharGenerator();
+        tempPass.push(randMasterChar);
+      }
+      password = tempPass.join("");
+      checkPassword();
+    } else {
+      showError();
+    }
+  } else {
+    showError();
+  }
 }
 
 function generatePassword() {
-  userPrompts();
-  masterArray();
   var tempPass = [];
   if (passwordLength > 7 && passwordLength < 129) {
     if (
@@ -183,22 +209,17 @@ function generatePassword() {
   }
 }
 
-// function checkPassword() {
-//   if (userSpecialCase) {
-//     for (var j = 0; j < password.length; j++) {
-//       var SpecialCaseTrue = specialChar.includes(password[j]);
-//       if (SpecialCaseTrue === false) {
-//         break;
-//       }
-//     }
-//     console.log(SpecialCaseTrue);
-//   }
-// }
+function showPassword() {
+  writePassword.value = password;
+}
 
-// generatePassword();
+function showError() {
+  writePassword.value = "Invalid Input. Hit Generate Password again";
+}
 
-// // Add event listener to generate button
 generateBtn.addEventListener("click", function () {
   writePassword.value = " ";
+  userPrompts();
+  masterArray();
   generatePassword();
 });
